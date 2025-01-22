0f18df28737ff027ef2420f69e3ab3182ebfd17c
Asbadsito <aswadsyednaz@gmail.com>
2025-01-22 04:19:39 +0000

    Solve bugs

--- FrontEnd/src/Components/Registration/ImageSlider.jsx
+++ FrontEnd/src/Components/Registration/ImageSlider.jsx
@@ -5,11 +5,13 @@ import 'swiper/css/bundle'; // This will import all Swiper styles, including Aut
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-fade'; // Import styles for fade effect
+import './slider.css';
 
 import { Autoplay, Pagination, Navigation, EffectFade } from 'swiper/modules'; // Import the fade effect
 
 const ImageSlider = () => {
   return (
+    <div className='w-full h-full'>
     <Swiper
       spaceBetween={0} 
       slidesPerView={1} 
@@ -18,7 +20,7 @@ const ImageSlider = () => {
         delay: 3000, 
         disableOnInteraction: false, 
       }}
-      speed={1500} 
+      speed={1000} 
       modules={[Autoplay]} 
       className="w-full h-full" 
     >
@@ -26,31 +28,32 @@ const ImageSlider = () => {
         <img
           src="/images/slider1.jpg"
           alt="Slider Image 1"
-          className="w-full h-full object-cover" 
+          className="w-full h-full object-cover shadow-right " 
         />
       </SwiperSlide>
       <SwiperSlide>
         <img
           src="/images/slider2.jpg"
           alt="Slider Image 2"
-          className="w-full h-full object-cover" 
+          className="w-full h-full object-cover shadow-right " 
         />
       </SwiperSlide>
       <SwiperSlide>
         <img
           src="/images/slider3.jpg"
           alt="Slider Image 3"
-          className="w-full h-full object-cover" 
+          className="w-full h-full object-cover shadow-right " 
         />
       </SwiperSlide>
       <SwiperSlide>
         <img
           src="/images/slider4.jpg"
           alt="Slider Image 4"
-          className="w-full h-full object-cover" 
+          className="w-full h-full object-cover shadow-right"
         />
       </SwiperSlide>
     </Swiper>
+    </div>
   );
 };
 
