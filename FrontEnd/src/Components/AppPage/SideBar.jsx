50968cbf943c998ab8b73dc71510b52661e79304
Asbadsito <aswadsyednaz@gmail.com>
2025-03-04 09:03:59 +0000

    Upgrade both sidebars

--- FrontEnd/src/Components/AppPage/SideBar.jsx
+++ FrontEnd/src/Components/AppPage/SideBar.jsx
@@ -4,15 +4,34 @@ import { useNavigate } from "react-router-dom";
 
 const SideBar = () => {
 
-  const[buttonSelected , setButtonSelected] = useState(null);
+  const[pageSelected , setPageSelected] = useState("home");
   const changeUrl = useNavigate();
    
   return (
     <div className='sidebarContainer  h-full w-44 hidden sm:hidden md:hidden lg:block lg: flex-col'>
-      <div className='barSelector w-full h-10 mt-6' onClick={() => changeUrl("/homePage")}>Homepage</div>
-      <div className='barSelector w-full h-10' onClick={() => changeUrl("/friendsPage")}>Friends</div>
-      <div className='barSelector w-full h-10'onClick={() => changeUrl("/recommendationPage")}>Recommendations</div>
-      <div className='barSelector w-full h-10 'onClick={() => changeUrl("/profilePage")}>Profile</div>
+      <div 
+        className={` w-full h-10 mt-4 ${pageSelected === "home" ? "selectedDiv" : "barSelector"}`}
+        onClick={() => { setPageSelected("home"); changeUrl("/homePage"); }}>
+          Homepage
+      </div>
+      
+      <div 
+        className={`w-full h-10 ${pageSelected === "friends" ? "selectedDiv" : "barSelector"}`}
+        onClick={() => {changeUrl("/friendsPage") ; setPageSelected("friends")}}>
+          Friends
+      </div>
+
+      <div 
+        className={`w-full h-10 ${pageSelected === "recommendation" ? "selectedDiv" : "barSelector"}`}
+        onClick={() => {changeUrl("/recommendationPage") ; setPageSelected("recommendation")}}>
+          Recommendations
+      </div>
+
+      <div 
+        className={`w-full h-10 ${pageSelected === "profile" ? "selectedDiv" : "barSelector"}`}
+        onClick={() => {changeUrl("/profilePage") ; setPageSelected("profile")}}>
+          Profile
+      </div>
     </div>
   )
 }
