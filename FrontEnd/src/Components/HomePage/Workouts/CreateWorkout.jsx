701c1f5cfd870253400543ef99f243086d1cf38a
Asbadsito <aswadsyednaz@gmail.com>
2025-03-12 02:07:55 +0000

    Almost finished with the workout Creation

--- FrontEnd/src/Components/HomePage/Workouts/CreateWorkout.jsx
+++ FrontEnd/src/Components/HomePage/Workouts/CreateWorkout.jsx
@@ -22,6 +22,7 @@ const CreateWorkout = ( {showCreateWorkout , setShowCreateWorkout , setOverlay} 
     const [sets, setSets] = useState(null);
     const [reps, setReps] = useState(null);
     const [duration, setDuration] = useState(null);
+    const [settingMessage , setSettingMessage] = useState("");
 
 
     const workoutTypes = [
@@ -35,11 +36,6 @@ const CreateWorkout = ( {showCreateWorkout , setShowCreateWorkout , setOverlay} 
     "STRENGTH"
     ];
 
-    useEffect (() => {
-
-    getAPIforExercises();
-    
-  } , [])
 
    useEffect(() => {
     fetch('/exercises/exercises.json')
@@ -51,33 +47,52 @@ const CreateWorkout = ( {showCreateWorkout , setShowCreateWorkout , setOverlay} 
 
     console.log(showCreateWorkout);
 
-    const getAPIforExercises = async () => {
 
-    const options = {
-      method: 'GET',
-      url: 'https://exercisedb.p.rapidapi.com/exercises',
-      params: {
-        limit: '0',
-        offset: '0'
-      },
-      headers: {
-        'x-rapidapi-key': '4cbea9721fmsh9fc17eb8027aab9p1f0303jsn0654b11c6c3b',
-        'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
+  const addExerciseToList = () => {
+    let message = "";
+
+    if(selectedExerciseImage == null && selectedExerciseName == null){
+      message = "Please select an exercise"
+      setSettingMessage(message);
+      console.log(message)
+      return;
+    }
+
+    if(selectedExerciseType === "TIMED"){
+      if(duration == null){
+        message = "Please select a duration"
+        setSettingMessage(message)
+        console.log(message)
+        return;
       }
+    }
+
+    if(selectedExerciseType === "SET_BASED"){
+      if(sets == null || reps == null){
+        message = "Please select your sets and repetitions"
+        setSettingMessage(message)
+        console.log(message)
+        return;
+      }
+    }
+
+    message = "Checkings have passed, object has been created, you can see it here : "
+
+    const newExercise = {
+      name: selectedExerciseName,
+      img_URL: selectedExerciseImage,
+      exerciseType: selectedExerciseType,
+      duration: selectedExerciseType === "TIMED" ? duration : null,
+      sets: selectedExerciseType === "SET_BASED" ? sets : null,
+      reps: selectedExerciseType === "SET_BASED" ? reps : null,
     };
 
-    try {
-      const response = await axios.request(options);
-      const filteredData = response.data.map(exercise => ({
-      name: exercise.name,
-      gifUrl: exercise.gifUrl 
-    }));
+    setListOfExerciseForWorkout((prevExercises) => [...prevExercises, newExercise]);
 
-    localStorage.setItem('exercises', JSON.stringify(filteredData));
-    } catch (error) {
-      console.error(error);
-    }
-    
+    console.log(newExercise);
+    console.log(listOfExerciseForWorkout)
+
+
   }
 
 
@@ -115,8 +130,8 @@ const CreateWorkout = ( {showCreateWorkout , setShowCreateWorkout , setOverlay} 
             container and not conditionally shown SHOULD NOT be below this part to keep it organsied,otherwise it would be too hard
             to read*/ }
 
-            <i class="close fa-solid fa-x absolute top-4 right-6" onClick={() => {setOverlay(prev => !prev) ; setShowCreateWorkout(false) ; setShowExercisePage(false)}}></i>
-            {showExercisePage && (<div className='excercisePage absolute w-[90%] h-[65%] border top-12 bg-white left-[5%] z-60 overflow-auto'>
+            <i class="close fa-solid fa-x absolute top-4 right-6" onClick={() => {setOverlay(prev => !prev) ; setShowCreateWorkout(false) ; setShowExercisePage(false) ; setListOfExerciseForWorkout([]);}}></i>
+            {showExercisePage && (<div className='excercisePage absolute w-[90%] h-[65%] border top-12 bg-gray-100 left-[5%] z-60 overflow-auto'>
               
                 <div className="exerciseGrid flex flex-wrap justify-center gap-4 p-4 ">
                     {exerciseData.map((exercise, index) => (
@@ -150,16 +165,76 @@ const CreateWorkout = ( {showCreateWorkout , setShowCreateWorkout , setOverlay} 
                 <span className='spanExName ml-4 h-[60px] w-28 text-[14px]'>{selectedExerciseName}</span>
                 <div className="absolute w-48 h-8 top-1 right-9 flex pl-8">
                   <div className={`px-2 py-1 text-[11px] w-36 h-6 bg-gray-100 text-black cursor-pointer hover:opacity-90 text-center ${selectedExerciseType === "SET_BASED" ? "text-white bg-blue-900" : ""}`}
-                       onClick={() => {setSelectedExerciseType("SET_BASED")}}
+                       onClick={() => {
+                        setSelectedExerciseType("SET_BASED");
+                        setDuration(null); 
+                        setSets(""); 
+                        setReps(""); 
+                      }}
                   >
                     SET BASED
                   </div>
                   <div className={`px-2 py-1 text-[11px] w-36 h-6 bg-gray-100 text-black  cursor-pointer hover:opacity-90 text-center ${selectedExerciseType === "TIMED" ? "text-white bg-blue-900" : ""}`}
-                       onClick={() => {setSelectedExerciseType("TIMED")}}
+                       onClick={() => {
+                        setSelectedExerciseType("TIMED");
+                        setSets(null); 
+                        setReps(null); 
+                        setDuration(""); 
+                       }}
                   >
                     TIMED
                   </div>
                 </div>
+
+                <div className='bg-red-700 text-white w-[18%] text-[11px] h-7 absolute bottom-2 right-24 text-center cursor-pointer hover:opacity-70 flex justify-center items-center'
+                      onClick={() => {setShowExercisePage(false) ; setSelectedExerciseImage(null) ; setSelectedExerciseName(null)}}
+                ><span>Cancel</span></div>
+
+                {selectedExerciseType === "TIMED" 
+                ? (<div className='absolute w-36 h-8 mt-2 left-3 bottom-3'>
+                     <input
+                      type="number"
+                      className="border rounded-lg p-2 w-[50%] outline-none 
+                      appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+                      placeholder="Duration"
+                      value={duration}
+                      onChange={(e) => setDuration(e.target.value)}
+                    />
+                    <span className='absolute ml-2 mt-2 text-[15px]'>s</span>
+                </div>) 
+                : (<div className='absolute w-36 h-8 mt-2 left-3 bottom-3'> 
+                    <input
+                      type="number"
+                      className="border rounded-lg p-2 w-[35%] outline-none mr-2
+                      appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+                      placeholder="Sets"
+                      value={sets}
+                      onChange={(e) => setSets(e.target.value)}
+                    />
+
+                    <input
+                      type="number"
+                      className="border rounded-lg p-2 w-[35%] outline-none  
+                      appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+                      placeholder="Reps"
+                      value={reps}
+                      onChange={(e) => setReps(e.target.value)}
+                    />
+                </div>)}
+
+                <button className="bg-blue-950 text-white w-[18%] h-7 absolute text-[11px] bottom-2 right-6 text-center cursor-pointer hover:opacity-70 flex justify-center items-center"
+                        onClick={() => {
+                          addExerciseToList();
+                          setShowExercisePage(false);
+                          setSelectedExerciseImage(null)
+                          setSelectedExerciseName(null)
+                          setSelectedExerciseType("SET_BASED")
+                          setSets(null)
+                          setReps(null)
+                          setDuration(null)
+                          setSettingMessage("")
+                        }}
+                > Add Exercise</button>
               </div>
 
             </div>)}
